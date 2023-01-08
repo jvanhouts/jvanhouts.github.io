7313f7a9d4b9bc88ab82673ec3ebd41da718701a
gunwunbun <jvanhouts@protonmail.com>
2023-01-08 19:48:53 +0100

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',  // path to public directory
+			assets: 'build',  // path to public directory
+			fallback: null
+		})
+	}
+};
+
+export default config;
