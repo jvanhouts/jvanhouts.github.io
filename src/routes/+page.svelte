731753bea8d5b8dc95f5041d132657fe973f40b6
gunwunbun <jvanhouts@protonmail.com>
2024-01-10 09:45:29 +0100

    redesign

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import ThemeToggle from "$lib/ThemeToggle.svelte";
     import { ArrowRight, Globe, ClipboardList } from 'lucide-svelte';
     import SlimeShooterImage from '$lib/assets/slime_shooter.png';
     import LizardLauncherImage from '$lib/assets/lizard_launcher.png';
@@ -8,12 +7,7 @@
 </script>
 
 <div class="w-full py-8 flex items-center justify-center">
-    <div class="w-11/12 md:w-3/4 2xl:w-1/2 max-w-screen-xl">
-        <div class="relative w-full flex justify-end bg-secondary-light dark:bg-secondary py-4 rounded-xl">
-            <ThemeToggle />
-            <a class="btn git rounded border mr-8" href="https://github.com/gunwunbun" target="_blank" rel="noreferrer">GitHub<ArrowRight class="ml-2 mb-1 inline" /></a>
-        </div>
-        
+    <div class="w-11/12 md:w-3/4 2xl:w-1/2 max-w-screen-xl">        
         <div class="flex flex-col justify-center items-center mt-16">
             <div class="flex flex-col justify-center items-center">
                 <img src="https://avatars.githubusercontent.com/u/34831190?v=3" class="w-1/4 lg:w-3/12 rounded-full" alt="Me" />
@@ -24,40 +18,32 @@
 </div>
 
 <div class="w-full px-0 pt-0 lg:pb-8 pb-8 flex items-center justify-center">
-    <div class="w-11/12 md:w-3/4 2xl:w-1/2 max-w-screen-xl">
-        <div class="mt-16">
-            <h2 class="font-semibold text-4xl">Recent Projects</h2>
+    <div class="w-11/12 xl:w-3/4 3xl:w-1/2 max-w-screen-xl">
+        <div class="mt-16 mb-8 pt-4 border-top-solid flex justify-between">
+            <h2 class="font-semibold text-3xl">Recent Projects</h2>
+            <a class="btn-border git rounded border" href="https://github.com/gunwunbun" target="_blank" rel="noreferrer">View more on GitHub<ArrowRight class="ml-2 mb-1 inline" /></a>
         </div>
 
         <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 min-h-fit">
-            <div class="relative bg-slime-shooter rounded-xl p-8 mt-8 card flex flex-col justify-between"on:click={ x => redirect("https://github.com/hollow-em/lizardLauncher") }>
-                <div class="absolute rounded-xl inset-0 w-full h-full bg-accent bg-opacity-95"></div>
-                <div class="z-10">
-                    <h1 class="title text-white">Slime Shooter</h1>
-                    <p class="mt-2 mb-4 leading-relaxed text-md text-gray-100 h-16">Top-Down Shooter game made with Unity, built for WebGL.</p>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 min-h-fit">
+            <div class="card" on:click={ x => redirect("https://simmer.io/@gunwunbun/slimeshooter") }>
+                <img src={SlimeShooterImage} alt="Slime Shooter cover art" class="lg:h-64 2xl:h-72 3xl:h-80">
+                <div class="bg-accent p-8 w-11/12 h-36">
+                    <h1 class="title text-white">Slime Shooter<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline lucide-icon lucide lucide-arrow-right arrow ml-1"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></h1>
+                    <p class="mt-2 mb-4 leading-relaxed text-lg text-gray-100">Top-Down Shooter game made with Unity, built for WebGL.</p>
                 </div>
-                <span class="mt-8 btn-inline flex items-center text-white mx-auto z-10" href="https://simmer.io/@gunwunbun/slimeshooter">Take me there <ArrowRight class="arrow ml-1" /></span>
             </div>
-            <div class="relative bg-lizard-launcher bg-cover rounded-xl p-8 lg:mt-8 card flex flex-col justify-between"on:click={ x => redirect("https://github.com/hollow-em/lizardLauncher") }>
-                <div class="absolute rounded-xl inset-0 w-full h-full bg-[#326470] bg-opacity-95"></div>
-                <div class="z-10">
-                    <h1 class="title text-white">Lizard Launcher <span class="text-gray-300 text-sm">(Closed Alpha)</span></h1>
-                    <p class="mt-2 mb-4 leading-relaxed text-md text-gray-100 h-16">Cataclysm: Dark Days Ahead launcher app built with Rust and SvelteKit.</p>
+            <div class="card" on:click={ x => redirect("https://github.com/hollow-em/lizardLauncher") }>
+                <img src={LizardLauncherImage} alt="Lizard Launcher cover art" class="lg:h-64 2xl:h-72 3xl:h-80">
+                <div class="bg-accent p-8 w-11/12 h-36">
+                    <h1 class="title text-white">Lizard Launcher <span class="text-gray-300 text-sm">(Closed Alpha)</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline lucide-icon lucide lucide-arrow-right arrow ml-1"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></h1>
+                    <p class="mt-2 mb-4 leading-relaxed text-lg text-gray-100">Cataclysm: Dark Days Ahead launcher app built with Rust and SvelteKit.</p>
                 </div>
-                <span class="mt-8 btn-inline flex items-center text-white mx-auto z-10" href="https://github.com/gunwunbun">Take me there <ArrowRight class="arrow ml-1" /></span>
-            </div>
-            <div class="bg-secondary-light rounded-xl p-8 card flex flex-col justify-between" on:click={ x => redirect("https://github.com/gunwunbun") }>
-                <div>
-                    <h1 class="title">My other work</h1>
-                    <p class="my-6 leading-relaxed text-xl">Check out my GitHub to see more of my projects.</p>
-                </div>
-                <span class="btn-inline flex items-center">Take me there <ArrowRight class="arrow ml-1"/></span>
             </div>
         </div>
 
-        <div class="mt-16">
-            <h2 class="font-semibold text-4xl">Working experience</h2>
+        <div class="mt-24 mb-8">
+            <h2 class="pt-4 border-top-solid font-semibold text-4xl">Working experience</h2>
         </div>
 
         <div class="mt-12 pl-8 border-l-4 border-secondary-light w-full lg:w-3/4 relative">
